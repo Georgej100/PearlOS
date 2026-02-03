9fca1f969c53213cf97ff555f2ec9cf9f7c4eac1
George Joseph <georgejoseph5767@gmail.com>
2026-02-03 20:51:05 +0000

    Loading kernel, but doing some research first

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 docker:
 	nasm boot/boot.s -f bin -o tmp/boot/boot.bin -I boot/
-	nasm boot/loader.s -f bin -o tmp/boot/loader.bin 
+	nasm boot/loader.s -f bin -o tmp/boot/loader.bin -I boot/ 
 	cat tmp/boot/boot.bin tmp/boot/loader.bin > out/bootloader.bin
 	qemu-system-x86_64 out/bootloader.bin -nographic
--- boot/loader.s
+++ boot/loader.s
@@ -1,5 +1,38 @@
 [org 0x7e00]
 
-jmp $
+mov dh, 8
+mov cl, 5
+mov dl, 0
+mov bx, where oh where  
+call read_disk 
+
+cli
+
+lgdt [gdt_descriptor]   ; From gdt.s
+
+mov eax, cr0 
+or eax, 1   ; Set first bit of CR0 , a control register
+mov cr0, eax
+
+jmp CODE_SEG:pm_mode_start
+
+%include "gdt.s"
+%include "disk.s"
+
+[bits 32]
+
+pm_mode_start:
+    mov ax, DATA_SEG ; Now in PM , our old segments are meaningless ,
+    mov ds, ax ; so we point our segment registers to the
+    mov ss, ax ; data selector we defined in our GDT
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    
+    mov ebp, 0x9fc00
+    mov esp, ebp
+
+    jmp $
+
 
 times 1536-($-$$) db 0 
